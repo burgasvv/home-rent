e134110e0bb87d08d93eba9b9589b4a70a586994
Burgas <burgassme@gmail.com>
2026-08-20 16:46:23 +0700

    Configure Kafka

--- src/main/kotlin/kafka/KafkaConsumer.kt
+++ src/main/kotlin/kafka/KafkaConsumer.kt
@@ -4,7 +4,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.burgas.dto.IdentityResponse
 import java.time.Duration
 
 fun CoroutineScope.configureKafkaConsumer() {
@@ -19,7 +21,13 @@ fun CoroutineScope.configureKafkaConsumer() {
         while (isActive) {
             val consumerRecords = kafkaConsumer.poll(Duration.ofMillis(10))
             consumerRecords.forEach { consumerRecord ->
-                println("${consumerRecord.topic()} :: ${consumerRecord.value()}")
+                when(consumerRecord.topic()) {
+                    KafkaTopics.identityTopic.name() -> {
+                        val identityResponse = Json.decodeFromString<IdentityResponse>(consumerRecord.value())
+                        println("${consumerRecord.topic()} :: ${consumerRecord.key()} :: $identityResponse")
+                    }
+                    else -> println("Wrong topic :: ${consumerRecord.topic()}")
+                }
             }
         }
     }
